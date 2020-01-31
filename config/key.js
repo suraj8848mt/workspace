f0515a754ba3d43c3b59143229393cf4f740e2f9
suraj.iwacademy <suraj.iwacademy@gmail.com>
2020-01-31 21:01:21 +0545

    Setting the mongodb connections

---
+++ config/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://root:root123@ds255917.mlab.com:55917/root'
+}
--- server.js
+++ server.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB config
+const db = require('./config/key').mongoURI;
+
+//connect to MongoDb
+
+
+
+
+mongoose
+    .connect(db, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+    .then(() => console.log('DB Connected!'))
+    .catch(err => {
+        console.log("DB Connection Error: ${err.message}");
+    });
 
 app.get('/', (req, res) => res.send('hello'))
 
