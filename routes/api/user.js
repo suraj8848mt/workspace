fbd519017d0050ed3f1eb99c16e87e23e7a77f3b
suraj.iwacademy <suraj.iwacademy@gmail.com>
2020-01-31 22:35:10 +0545

    created register api

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 // Load User model
 const User = require('../../models/User')
     // @route GET api/users/test
@@ -17,11 +19,28 @@ router.post('/register', (req, res) => {
         if (user) {
             return res.status(400).json({ email: 'Email already exists' })
         } else {
+            const avatar = gravatar.url(req.body.email, {
+                s: '200', //size
+                r: 'pg', //Rating
+                d: 'mm' // default
+
+            })
             const newUser = new User({
                 name: req.body.name,
                 email: req.body.email,
                 avatar: avatar,
                 password: req.body.password
+            });
+
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                        .then(user => res.json(user))
+                        .catch(err => console.log(err));
+
+                })
             })
         }
     })
